d38cf5434759e77f1da62be37f6c751e9d7cd64e
yashkumbhani1147 <yashkumbhani1147@example.com>
2022-09-27 09:56:57 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:ads/banner_ads.dart';
+import 'package:flutter/material.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  MobileAds.instance.initialize();
+  runApp(MaterialApp(home: ads(),));
+}
+class ads extends StatefulWidget {
+  const ads({Key? key}) : super(key: key);
+
+  @override
+  State<ads> createState() => _adsState();
+}
+
+class _adsState extends State<ads> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Advertisment"),),
+      body: Center(
+        child: Column(
+          children: [
+            ElevatedButton(onPressed: () {
+              Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) {
+                return banner_ads();
+              },));
+            }, child: Text("Baner Ads")),
+
+          ],
+        ),
+      ),
+    );
+  }
+}
